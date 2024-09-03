b22f48bdbb31c28c2c40cd33e61e8b628500b792
Soumyajit Naskar <soumyajit13102003@gmail.com>
2024-09-04 02:40:32 +0530

    Initial commit

--- .ccls-cache/@home@runner@E-Visa-Management-System/document.cpp
+++
@@ -1,9 +0,0 @@
-#include "document.hpp"
-
-// Document(const string& documentType, const string& documentPath);
-Document::Document(const string &documentType, const string &documentPath)
-    : documentType(documentType), documentPath(documentPath) {}
-
-string Document::getDocumentType() const { return documentType; }
-
-string Document::getDocumentPath() const { return documentPath; }
--- .ccls-cache/@home@runner@E-Visa-Management-System/notification.cpp
+++
@@ -1,6 +0,0 @@
-#include "notification.hpp"
-
-Notification::Notification(const string& message) : message(message) {}
-
-string Notification::getMessage() const { return message; }
-
